06531c1a2878f1efb4ec76c666ed16284dc8f2c0
xiaoLin12130 <1050601448@qq.com>
2026-08-11 10:32:30 +0800

    chore: 清理遗留探测脚本, PROGRESS收尾快照+明日待办

--- scripts/tests/_probe_engine.gd
+++
@@ -1,9 +0,0 @@
-extends SceneTree
-## 临时探测：Godot 4.7 是否支持 Engine.print_error_messages / get_error_messages
-
-func _init() -> void:
-	Engine.print_error_messages = true
-	push_error("PROBE_MARKER")
-	var m: String = Engine.get_error_messages()
-	print("CAPTURED_MESSAGES=[", m, "]")
-	quit(0)
